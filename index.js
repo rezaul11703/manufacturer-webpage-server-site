64d132ae61e267824f50457d3015eefbc9f9de90
rezaul11703 <rezaulkarim11703@gmail.com>
2022-05-29 14:46:13 +0600

    some changes in crud

--- index.js
+++ index.js
@@ -130,12 +130,7 @@ async function run() {
         res.status(403).send({ message: "Forbidden" });
       }
     });
-    app.get("/admin/:email", async (req, res) => {
-      const email = req.params.email;
-      const user = await usersCollection.findOne({ email: email });
-      const isAdmin = user.role === "admin";
-      res.send({ admin: isAdmin });
-    });
+
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
